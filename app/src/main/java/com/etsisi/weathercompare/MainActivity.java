7b90940dfa68fdb2b4284a8a64c8c91598321006
Ana <20310114+anamarych@users.noreply.github.com>
2021-06-11 22:49:10 +0200

    Project start

---
+++ app/src/main/java/com/etsisi/weathercompare/MainActivity.java
@@ -0,0 +1,172 @@
+package com.etsisi.weathercompare;
+
+//API model
+import com.etsisi.weathercompare.models.Weather;
+
+//android
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//firebase
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+//retrofit
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//java
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String LOG_TAG = "WeatherApp";
+    private static final String API_BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private TextView countryResult;
+    private EditText etCountryName;
+
+    private ICountryRESTAPIService apiService;
+
+    private static final int RC_SIGN_IN = 2020;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.logoutButton).setOnClickListener(this);
+        countryResult = findViewById(R.id.countryResult);
+        etCountryName = findViewById(R.id.countryName);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        apiService = retrofit.create(ICountryRESTAPIService.class);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in
+                    CharSequence username = user.getDisplayName();
+                    Toast.makeText(MainActivity.this, getString(R.string.firebase_user_fmt, username), Toast.LENGTH_LONG).show();
+                    Log.i(LOG_TAG, "onAuthStateChanged() " + getString(R.string.firebase_user_fmt, username));
+                    ((TextView) findViewById(R.id.cityView)).setText(getString(R.string.currentcity, "Current"));
+
+                } else {
+                    // user is signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */)
+                                    .setLogo(R.drawable.map_point)
+                                    .build(),
+                            RC_SIGN_IN
+                    );
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, R.string.signed_in, Toast.LENGTH_SHORT).show();
+                Log.i(LOG_TAG, "onActivityResult " + getString(R.string.signed_in));
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, R.string.signed_cancelled, Toast.LENGTH_SHORT).show();
+                Log.i(LOG_TAG, "onActivityResult " + getString(R.string.signed_cancelled));
+                finish();
+            }
+        }
+    }
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        mFirebaseAuth.signOut();
+        Log.i(LOG_TAG, getString(R.string.signed_out));
+    }
+
+    public void onClickMap(View v) {
+        //Open new activity
+        Intent activity2Intent = new Intent(getApplicationContext(), LocationActivity.class);
+        startActivity(activity2Intent);
+
+    }
+
+    public void getCountryByName(View v){
+        String countryName = etCountryName.getText().toString();
+        Log.i(LOG_TAG, "getCountryByName =" + countryName);
+        countryResult.setText("");
+
+        Call<List<Weather>> call_async = apiService.getWeather(countryName, "8dedcc7198c51b7d7119a0c408ce01fd");
+        call_async.enqueue(new Callback<List<Weather>>() {
+
+            @Override
+            public void onResponse(Call<List<Weather>> call, Response<List<Weather>> response) {
+                List<Weather> countryList = response.body();
+                int contPais = 0;
+                if (null != countryList) {
+                    for (Weather country : countryList) {
+                        contPais++;
+                        countryResult.append(contPais+ " - ["+country.getDescription() + "] "+"\n\n");
+                    }
+                    Log.i(LOG_TAG, "getCountryByName => respuesta=" + countryList);
+                } else {
+                    countryResult.setText(getString(R.string.strError));
+                    Log.i(LOG_TAG, getString(R.string.strError));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Weather>> call, Throwable t) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "ERROR: " + t.getMessage(),
+                        Toast.LENGTH_LONG
+                ).show();
+                Log.e(LOG_TAG, t.getMessage());
+            }
+
+
+        });
+    }
+}
